df64245c3e5235c7181b276a15297852c1a74ad0
liukai <liukai@istring.vip>
2022-08-14 15:23:02 +0800

    remove commit lines.

--- App/App.cpp
+++ App/App.cpp
@@ -44,19 +44,6 @@ int SGX_CDECL main(int argc, char *argv[])
     const std::string LIST_PORT = "40000";
     const std::string log_dir = "/root/glog-arweave";
     std::string input_line;
-    //
-    std::string test_req = "{" \
-    "\"userPublicKeyList\": ["  \
-    "\"049f992995affb335b576a7186316fc0ecfcca3d88f78dfb00e0e76e1f9a9766135230831442e4b1975f2caf81756a250032ea5e165ba1631606795be04a00d42c\", "\
-    "\"04e30cd9f1283b95251e2721ee6f1fcbbc6ea56f32c924c0000f6f4e6a91d474dd1ff40d39fb8601b4b4066027952ede10e2d144f1b3aa5b2b1bf4210f4cc93e3d\", "\
-    "\"0424c0853bdcb04fb8d50eaaa779f2c0d5f01c79b30b58f6a2fe739070e236cd142e32b8114f06b60b46b00f39745c874e8297ec9da01366927ac199072a103356\"  "\
-    "], "\
-    "\"k\": 2, "\
-    "\"l\": 3, "\
-    "\"keyLength\": 1024, "\
-    "\"callBackAddress\": \"http://127.0.0.1:8008/sayHi\" "\
-    "}";
-    //
 
     /** Initialize log */
     TeeLogHelper* glog_helper = nullptr;
@@ -84,28 +71,6 @@ int SGX_CDECL main(int argc, char *argv[])
         return -1;
     }
     
-#if 0
-    ////////////////////////Test/////////////////////////////
-    char* reply = nullptr;
-    size_t reply_len = 0;
-    sgx_status = ecall_run( global_eid, &ret, 101,  "1111", test_req.c_str(), test_req.length(), &reply, &reply_len );
-    if (sgx_status != SGX_SUCCESS || ret != 0) {
-        printf( "ecall_run() failed, ret: 0x%x, error message: %s\n", ret, t_strerror((int)sgx_status) );
-        delete glog_helper;
-        return -1;
-    }
-    printf( "--->reply lenght: %d\n", (int)reply_len );
-    printf( "--->reply: %s\n", reply );
-    free( reply );
-    reply = nullptr;
-    sgx_status = ecall_free( global_eid );
-    if ( sgx_status != SGX_SUCCESS ) {
-        printf( "ecall_free() failed, error message: %s\n", t_strerror((int)sgx_status) );
-        delete glog_helper;
-        return -1;
-    }
-    ////////////////////////Test/////////////////////////////
-#endif //0
     /** Initializing the REST service listener */
     listen_addr.append(":");
     listen_addr.append(LIST_PORT);
